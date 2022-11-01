6393b65e7786706fa48dc8d6cceff032b31c4f55
Vitalii9907 <111561567+Vitalii9907@users.noreply.github.com>
2022-11-01 23:34:47 +0200

    prepares hero section

---
+++ src/js/swiper-initial.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
