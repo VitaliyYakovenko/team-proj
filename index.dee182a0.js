4f9272c249197c0a3a04aec4a44fe970bacd1a2b
VitaliyYakovenko <111122626+VitaliyYakovenko@users.noreply.github.com>
2023-06-15 08:38:09 +0000

    Deploying to gh-pages from @ VitaliyYakovenko/team-proj@c7b775d961a841bbcc9abcfb232e23ae94f22e28 🚀

---
+++ index.dee182a0.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,autoplay:{delay:3e3,disableOnInteraction:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.dee182a0.js.map
